ebab6ac0cdcbc4912057fd026a9fb1222ebfb6d8
Xan <iamxanng@gmail.com>
2024-05-14 00:18:56 +0800

    Updated Signup page and Login page

--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -10,14 +10,6 @@ const SignupPage = () => {
         <div className='flex h-dvh justify-center items-center'>
             <div className="flex flex-col w-[400px] gap-[20px] justify-center items-center">
                 <SignupForm />
-                <div className={style}>or continue with</div>
-                <Button type="button" className="bg-[#E6E6E6] text-black w-full">
-                    <FcGoogle className="mr-2 h-4 w-4" />Google
-                </Button>
-                <div className={style}>By clicking continue, you agree to our 
-                    <a href='' className='text-black'> Terms of Service</a> and 
-                    <a href='' className='text-black'> Privacy Policy</a>
-                </div>
             </div>
         </div>
     )
