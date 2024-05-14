0457028052c24eacc23bba593f8861347f62d8a4
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-05-14 18:22:04 +0800

    completed backend Authentication

---
+++ backend/src/routes/courses.routes.ts
@@ -0,0 +1,11 @@
+import express, { Router } from "express";
+import AuthController from "../controllers/auth.controller.js";
+import passport from "passport";
+
+const courseRouter: Router = express.Router();
+
+courseRouter.get("/protected", passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.status(200).json({msg: "Authorized!"});
+})
+
+export default courseRouter;
