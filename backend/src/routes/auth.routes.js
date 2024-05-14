005196b5fe377b0e896bb34c188261207fd279ef
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-05-14 17:28:26 +0800

    Removed all JS files from backend

--- backend/src/routes/auth.routes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import AuthController from "../controllers/auth.controller.js";
-const authRouter = express.Router();
-authRouter.route("/login").post(AuthController.apiLogin);
-authRouter.route("/signup").post(AuthController.apiCreateUser);
-export default authRouter;
