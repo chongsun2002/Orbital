1cc64de93c79632d4595f79f3612635287f956be
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-05-17 02:55:54 +0800

    fixed login, logout still broken

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/ui/navbar";
+import { cookies } from "next/headers";
 
 export default async function Home() {
   return (
-    <Navbar />
+    <Navbar userName={cookies().get('userName')?.value || ""}/>
   )}
