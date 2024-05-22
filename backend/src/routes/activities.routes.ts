238e250e2a2ce38543c71ad3c12819334f32ca20
Xan <iamxanng@gmail.com>
2024-05-22 21:12:11 +0800

    Fixed create activity functionality

--- backend/src/routes/activities.routes.ts
+++ backend/src/routes/activities.routes.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import ActivitiesController from "../controllers/activities.controller.js";
+import passport from "passport";
 
 const activitiesRouter: Router = express.Router();
 
-activitiesRouter.route("/create").post(ActivitiesController.apiCreateActivity);
+activitiesRouter.route("/create").post(passport.authenticate('jwt', {session: false}), ActivitiesController.apiCreateActivity);
 
 export default activitiesRouter;
