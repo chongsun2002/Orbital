1d30bb5c576109e54dc5bb8130b67b52e436fa10
Xan <iamxanng@gmail.com>
2024-05-23 18:20:07 +0800

    Fixed functionality for search activities and display activities feature

--- backend/src/routes/activities.routes.ts
+++ backend/src/routes/activities.routes.ts
@@ -9,5 +9,6 @@ activitiesRouter.route("/search").post(ActivitiesController.apiSearchActivity);
 activitiesRouter.route("/join").post(passport.authenticate('jwt', {session: false}), ActivitiesController.apiAddParticipant);
 activitiesRouter.route("/leave").post(passport.authenticate('jwt', {session: false}), ActivitiesController.apiRemoveParticipant);
 activitiesRouter.route("/delete").post(passport.authenticate('jwt', {session: false}), ActivitiesController.apiDeleteActivity);
+activitiesRouter.route("/display").post(ActivitiesController.apiDisplayActivities);
 
 export default activitiesRouter;
