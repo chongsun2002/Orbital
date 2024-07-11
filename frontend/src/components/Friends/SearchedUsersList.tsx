8e5b61d4845da9c07adb8fbdcc7c55f5aadd0dbe
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-07-12 00:44:17 +0800

    fixed search user displaying self issue

--- frontend/src/components/Friends/SearchedUsersList.tsx
+++ frontend/src/components/Friends/SearchedUsersList.tsx
@@ -1,12 +1,16 @@
 import { SearchedUserDetails } from "@/lib/types/userTypes"
 import SearchedUserCell from "./SearchedUserCell"
 import { Separator } from "../ui/separator";
+import { cookies } from "next/headers";
 
 type SearchedUsersListProps = {
     users: SearchedUserDetails[];
 }
 
 const SearchedUsersList: React.FC<SearchedUsersListProps> = ({ users } : SearchedUsersListProps) => {
+    const session = cookies().get('session')?.value;
+    const username = session ? JSON.parse(session).name : "";
+    
     return (
         <div className="flex flex-col border-solid rounded-md">
             <Separator className="mb-2"/>
@@ -14,7 +18,7 @@ const SearchedUsersList: React.FC<SearchedUsersListProps> = ({ users } : Searche
                 Search Results
             </div>
             {users.map((user, index) => {
-                return (<SearchedUserCell key={index} id={user.id} name={user.name}/>)
+                return user.name === username ? null : (<SearchedUserCell key={index} id={user.id} name={user.name}/>)
             })}
         </div>
     )
